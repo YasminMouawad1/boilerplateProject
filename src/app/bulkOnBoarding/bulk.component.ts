be235df310edef50651f61d992e1b30bd1a02bee
YasminMouawad1 <111860564+YasminMouawad1@users.noreply.github.com>
2023-03-12 12:02:35 +0200

    edit

--- src/app/bulkOnBoarding/bulk.component.ts
+++ src/app/bulkOnBoarding/bulk.component.ts
@@ -82,7 +82,7 @@ export class BulkComponent extends PagedListingComponentBase<ApplicationsOnBoard
       allowClear: true
     };
 
-    this.getAllCorporates();
+    //this.getAllCorporates();
   }
 
 
